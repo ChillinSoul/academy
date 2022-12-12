cb1817a63d0f351516326cf6f8e930e9dfd47ef4
ChillinSoul <axelrlbergiers@gmail.com>
2022-12-12 16:30:12 +0000

    server running

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+let express = require("express")
+let app = express()
+app.set('view engine', 'ejs')
+
 app.get('/',(req,res)=>{
 	res.render('index.ejs')
+})
+app.listen(80,()=>{
+	console.log("running")
 })
